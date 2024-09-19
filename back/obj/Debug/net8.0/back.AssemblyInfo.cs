fa3849502410cb557475cac0aaf2f2fd1d6cd12f
itu-itis22-kuyumcus22 <kuyumcus22@itu.edu.tr>
2024-09-19 22:53:17 +0300

    final commit

--- back/obj/Debug/net8.0/back.AssemblyInfo.cs
+++ back/obj/Debug/net8.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ded740babb71d4155e4da629bb85a608dbd7b3af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef5a4be0d7d822b8cd4d2794ecc0903db7d7e7cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
