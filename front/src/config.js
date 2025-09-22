66c0bf2a6ccd30335fc004fa62e7de4bd23c11e3
itu-itis22-kuyumcus22 <kuyumcus22@itu.edu.tr>
2025-09-23 00:16:09 +0300

    newurl

--- front/src/config.js
+++ front/src/config.js
@@ -1,9 +1,7 @@
 let API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-// If no env variable is set, try to fetch the current ngrok URL
 if (!API_BASE_URL) {
-  // Fallback to a known URL - update this manually when needed
-  API_BASE_URL = 'https://mighty-lemons-doubt.loca.lt/ch';
+  API_BASE_URL = 'https://9063448c680d5a62135cbc0c64904cd4.serveo.net/ch';
 }
 
 export default API_BASE_URL;
