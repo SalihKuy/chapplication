61a6918348851b771224243c81a72af8e970f289
itu-itis22-kuyumcus22 <kuyumcus22@itu.edu.tr>
2025-09-24 15:35:14 +0300

    newurl

--- front/src/config.js
+++ front/src/config.js
@@ -1,7 +1,7 @@
 let API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 if (!API_BASE_URL) {
-  API_BASE_URL = 'https://9063448c680d5a62135cbc0c64904cd4.serveo.net/ch';
+  API_BASE_URL = 'https://ead402a9be8f.ngrok-free.app/ch';
 }
 
 export default API_BASE_URL;
